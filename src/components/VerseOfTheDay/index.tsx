d6516a6e14bf9fc7e2212bb9e5f980be42fcf69f
gabrielcaamargo <gcamargo.dev@gmail.com>
2022-12-09 16:33:13 -0300

    📦 chore: translate verse of the day

--- src/components/VerseOfTheDay/index.tsx
+++ src/components/VerseOfTheDay/index.tsx
@@ -1,10 +1,18 @@
+import { useTranslate } from '../../hooks/useTranslate';
 import { Container } from './styles';
 
 export default function VerseOfTheDay() {
   return (
     <Container>
-      <p className='title'><strong>Verse of the day</strong></p>
-      <p className='description'><strong>Gen 1:1 -</strong> In the beginning God created the heavens and the earth. </p>
+      {useTranslate(
+        <p className='title'><strong>Verse of the day</strong></p>,
+        <p className='title'><strong>Versículo do dia</strong></p>
+      )}
+
+      {useTranslate(
+        <p className='description'><strong>Gen 1:1 -</strong> In the beginning God created the heavens and the earth. </p>,
+        <p className='description'><strong>Gen 1:1 -</strong> No princípio Deus criou os céus e a terra. </p>
+      )}
     </Container>
   );
 }
