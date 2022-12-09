b46a5836ed18c8eec99f96a1ea10f5ac17e9b702
gabrielcaamargo <gcamargo.dev@gmail.com>
2022-12-09 16:31:24 -0300

    🔧 fix: improve useTranlsate using for a less verbose form

--- src/components/PlanBanner/index.tsx
+++ src/components/PlanBanner/index.tsx
@@ -1,12 +1,12 @@
+import { useTranslate } from '../../hooks/useTranslate';
 import { Container } from './styles';
 
 export default function PlanBanner( ) {
   return (
     <Container>
-      {/* <div className="overlay"></div> */}
       <div className="container-text">
-        <p>New converted?</p>
-        <p>Try a Bible reading plan to know more about God!</p>
+        {useTranslate(<p>New converted?</p>, <p>Novo convertido?</p>)}
+        {useTranslate(<p>Try a Bible reading plan to know more about God!</p>, <p>Teste um plano de leitura Bíblico para aprender mais sobre Deus!</p>)}
       </div>
     </Container>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,14 +6,13 @@ import VerseOfTheDay from '../../components/VerseOfTheDay';
 import { Container } from './styles';
 
 export default function Home() {
-  const translatedText: React.ReactNode = useTranslate(
-    <h2>Bringing you <span>closer</span> to God. Step by step. Day by day. </h2>,
-    <h2>Levando você <span>mais próxim</span> de Deus. Passo por passo. Dia por dia. </h2>
-  );
 
   return (
     <Container>
-      {translatedText}
+      {useTranslate(
+        <h2>Bringing you <span>closer</span> to God. Step by step. Day by day. </h2>,
+        <h2>Levando você <span>mais próxim</span> de Deus. Passo por passo. Dia por dia. </h2>
+      )}
       <PlanBanner />
       <VerseOfTheDay />
       <ReadNow />
